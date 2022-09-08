60ca72ec44d3b081c40a80f0bccec818b668b20c
task4233 <29667656+task4233@users.noreply.github.com>
2022-09-08 21:52:13 +0900

    [wip]

--- src/store/svModel.ts
+++ src/store/svModel.ts
@@ -34,6 +34,7 @@ export const svModelStore: VoiceVoxStoreOptions<
           // Select and load a ZIP File for sound library.
           filePath = await window.electron.showImportFileDialog({
             title: "サウンドライブラリ用ファイル(.svlib)の選択",
+            // filters: [{ name: ".svlib file", extensions: ["svlib"] }],
             filters: [{ name: ".svlib file", extensions: ["svlib"] }],
           });
           if (!filePath) return;
@@ -42,21 +43,43 @@ export const svModelStore: VoiceVoxStoreOptions<
         const projectFileErrorMsg = `The sound library file "${filePath}" is a invalid file.`;
 
         try {
-          // 音声ライブラリのZIPファイルをロード
           const buf = await window.electron.readFile({ filePath });
-
-          // ZIPファイルの解凍と型変換
-          const jsZip = await JSZip.loadAsync(buf);
+          const zip = await JSZip.loadAsync(buf);
           const svModelInfoObj = <SVModelInfo>{};
-          jsZip.forEach(async (relativePath, zipObject) => {
-            // WinやMacといった環境差異を吸収するためにpathライブラリを利用する
-            // ['3c888f14-b4b8-11ec-89e6-0242ac1c0002', 'voice_samples', '1758075148_003.wav']
-            // separatedPath[0]はspeaker_uuidになる
-            // separatedPath[1]がiconsの時はiconを想定してseparatedPath[2]を読む
-            // separatedPath[1]がvoice_samplesの時は音声サンプルを想定してseparatedPath[2]を読む
-            // それ以外はseparatedPath[1]を読む
-            //
-            // 想定ファイルたち
+
+          zip.forEach(async (relativePath, zipObject) => {
+            // ignore environment differences such as Win/Mac/Linux
+            // e.g.) ['3c888f14-b4b8-11ec-89e6-0242ac1c0002', 'voice_samples', '1758075148_003.wav']
+            // separatedPath[0]                   => should be speaker_uuid
+            // separatedPath[1] is icons          => should be icon, so read separatedPath[2]
+            // separatedPath[1] is voice_samples  => should be wav data, so read separatedPath[2]
+            // others                             => read separatedPath[1](should be file name)
+            const separatedPath = relativePath.split(path.sep);
+            if (separatedPath.length == 0) {
+              console.error(`${filePath} should be invalid`);
+              return;
+            }
+
+            const fileExt: string = path.extname(relativePath);
+            let convertType: JSZip.OutputType;
+            switch (fileExt) {
+              case ".png":
+              case ".onnx":
+              case ".wav":
+                convertType = "base64";
+                break;
+              default:
+                convertType = "string";
+                break;
+            }
+
+            const speaker_uuid: string = separatedPath[0];
+            zipObject.async(convertType).then((data) => {
+              console.log(separatedPath);
+              console.log(data);
+            });
+
+            // given files
             // - variance_model
             // - embedder_model
             // - decoder_model
@@ -69,34 +92,14 @@ export const svModelStore: VoiceVoxStoreOptions<
             //         <id>_00<n>.wav
             //       ]
             //     }
-            const separatedPath = relativePath.split(path.sep);
-            // base64エンコードする
-            // 似てるコードはプロジェクト内から探す
 
-            // ref: audio.ts#1288
-            // const base64Encoder = (blob: Blob): Promise<string | undefined> => {
-            //     return new Promise((resolve, reject) => {
-            //       const reader = new FileReader();
-            //       reader.onload = () => {
-            //         // string/undefined以外が来ることはないと思うが、型定義的にArrayBufferも来るので、toStringする
-            //         const result = reader.result?.toString();
-            //         if (result) {
-            //           // resultの中身は、"data:audio/wav;base64,<content>"という形なので、カンマ以降を抜き出す
-            //           resolve(result.slice(result.indexOf(",") + 1));
-            //         } else {
-            //           reject();
-            //         }
-            //       };
-            //       reader.readAsDataURL(blob);
-            //     });
-            //   };
-
-            console.log(separatedPath);
-            console.log(zipObject);
+            // console.log(separatedPath);
+            // console.log(zipObject);
+            // TODO: read data from zipObject
           });
 
-          console.log(svModelInfoObj);
-          console.log(confirm);
+          // console.log(svModelInfoObj);
+          // console.log(confirm);
 
           // setするとき
           context.commit("SET_SV_MODEL_INFO", { svModelInfo: svModelInfoObj });
